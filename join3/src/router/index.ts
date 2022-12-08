97a92d15afa3d937f5936eed2c9c249d9a9c5488
zhangxueyu <1501109542@qq.com>
2022-12-08 13:10:00 +0800

    feat: react-router

---
+++ join3/src/router/index.ts
@@ -0,0 +1,9 @@
+import { RouteObject } from 'react-router-dom'
+
+const routes = [
+  {
+    path: '/'
+  }
+] as RouteObject[]
+
+export default routes
--- join3/vite.config.ts
+++ join3/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   plugins: [react()]
 })
